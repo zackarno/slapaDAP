922706ce07b36c74ca84bd36d7880a3f0a5c45dd
zackarno <zackarno@gmail.com>
2021-01-22 13:24:01 +0300

    import_tool_dictionary inception

---
+++ R/import_tool_dictionary.R
@@ -0,0 +1,7 @@
+#' Import commonly used questions and corresponding options
+#' Can import individually or by groups (i.e metadata, washington group)
+
+
+# import_tool_dictionary<- function(){
+#
+# }
